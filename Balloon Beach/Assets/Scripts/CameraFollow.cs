dec32384f87af58410d122c2f50ee7e8196081c1
Slavi Boshnakov <43034168+slaviboshnakov@users.noreply.github.com>
2020-10-19 18:14:21 +0100

    1.1
    Many updates

---
+++ Balloon Beach/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject player;
+
+    void Update()
+    {
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position,
+            new Vector3(0, gameObject.transform.position.y, player.gameObject.transform.position.z - 10)
+            , Time.deltaTime * 100);
+    }
+}
